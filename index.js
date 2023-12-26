b79cb25077db87abd430c2aa30bad125db466393
timlucmiptev <info@timlucmiptev.space>
2023-12-26 21:17:23 +0100

    sorting

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('index.js');
+
+const mkSig = (data) => {
+  // sort fields in data by key, alphabetically
+  const sorted = Object.keys(data).sort().reduce((acc, key) => {
+    acc[key] = data[key];
+    return acc;
+  }, {});
+  return sorted;
+};
+
+console.log(mkSig({b: 2, a: 9, c: 3}));
