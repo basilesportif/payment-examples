ecf7ef02f5e57183aac29d014a24f54b274489e7
timlucmiptev <info@timlucmiptev.space>
2023-12-26 21:23:40 +0100

    signing works

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const crypto = require('crypto');
 
-const mkSig = (data) => {
-  // sort fields in data by key, alphabetically
-  const sorted = Object.keys(data).sort().reduce((acc, key) => {
-    acc[key] = data[key];
+// implements:  https://docs.fondy.eu/en/docs/page/3/#chapter-3-5
+const sign = (req) => {
+  const sorted = Object.keys(req).sort().reduce((acc, key) => {
+    acc[key] = req[key];
     return acc;
   }, {});
-  return sorted;
+  const joined = 'test|' + Object.keys(sorted).map(key => sorted[key]).join('|');
+  const hash = crypto.createHash('sha1');
+  hash.update(joined);
+  return hash.digest('hex');
 };
 
-console.log(mkSig({b: 2, a: 9, c: 3}));
+// "signature":"df38818facfbfd79953fa847667dac73a1291127",
+const req = {
+    "order_id":"test123456",
+    "order_desc":"test order",
+    "currency":"USD",
+    "amount":"125",
+    "merchant_id":"1396424"
+  }
+
+console.log(sign(req));
