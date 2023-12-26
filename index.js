9b3fbd07bcea87f789bbe89c690ad7c86624c074
timlucmiptev <info@timlucmiptev.space>
2023-12-26 22:58:05 +0100

    clarify pattern B

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const getSignature = (req) => {
   return { ...sorted, signature: hash.digest('hex') };
 };
 
-const req = {
+const genReq = () => {
+  return {
   "order_id": uuidv4(),
   "order_desc":"test order 2",
   "currency":"USD",
@@ -22,10 +23,11 @@ const req = {
   "merchant_id":"1396424",
   "server_callback_url":"http://myshop/callback/",
   "response_url":"http://myshop/return/",
+  }
 }
 
 // signature: 91ea7da493a8367410fe3d7f877fb5e0ed666490
-const req1 = {
+const staticReq = {
   "server_callback_url": "http://myshop/callback/",
   "order_id": "TestOrder2",
   "currency": "USD",
@@ -34,15 +36,26 @@ const req1 = {
   "amount": "1000",
 }
 
-console.log(getSignature(req));
+/*
+// Interaction A: TODO doesn't work with these params
+var res = 
+fetch('https://pay.fondy.eu/api/checkout/redirect', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({request: getSignature(genReq())})
+});
+res.then(r => r.text()).then(console.log);
+*/
 
-const res =
+// Interaction B
+var res =
 fetch('https://pay.fondy.eu/api/checkout/url', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
-  body: JSON.stringify({request: getSignature(req)}),
+  body: JSON.stringify({request: getSignature(genReq())}),
 });
 res.then(r => r.json()).then(console.log);
-// https://pay.fondy.eu/api/checkout/url
