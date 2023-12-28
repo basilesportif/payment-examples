95fe8fb70e94777a9dae6b2815caf30fbf3acd29
timlucmiptev <info@timlucmiptev.space>
2023-12-28 21:16:56 +0100

    create stylist button

--- stripe.js
+++ stripe.js
@@ -37,7 +37,38 @@ const createAccount = async ({email, country}) => {
 const runServer = () => {
   const app = express();
   const port = PORT;
+  const domain = `http://localhost:${port}`;
+  app.use(express.urlencoded({ extended: true }));
 
+  app.get('/', (req, res) => {
+    console.log('homepage');
+    res.send(
+      `<html><body>
+      <form action="${domain}/create_stylist" method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="country">Country:</label>
+        <select id="country" name="country" required>
+          <option value="UA">Ukraine</option>
+          <option value="US">USA</option>
+          <option value="KZ">Kazakhstan</option>
+        </select>
+        <input type="submit" value="Create Stylist">
+      </form>
+      </body></html>`
+    );
+  });
+  app.post('/create_stylist', async (req, res) => {
+    console.log('create_stylist');
+    console.log(req.body);
+    const { email, country } = req.body;
+    /*
+    const account = await createAccount({ email, country });
+    console.log(account);
+    */
+    res.send(`create_stylist ${email} ${country}`);
+  });
   app.get('/reauth/:accountId', (req, res) => {
     console.log('reauth');
     console.log(req.params.accountId);
@@ -50,9 +81,23 @@ const runServer = () => {
   });
 
   app.listen(port, () => {
-    console.log(`Stripe app listening at http://localhost:${port}`);
+    console.log(`Stripe app listening at ${domain}`);
   });
 };
+
+const printAccounts = (accounts) => {
+  console.log(accounts.data.map(account => [ 
+    account.id,
+    account.email,
+    account.country,
+    account.requirements.disabled_reason,
+    account.requirements.current_deadline,
+    account.requirements.past_due,
+    account.requirements.currently_due,
+    account.requirements.eventually_due,
+  ]));
+};
+
 
 const run = async () => {
   const accountId = 'acct_1OSLWXH8xIAoOsZC';
@@ -65,7 +110,6 @@ const run = async () => {
   console.log(accountLink);
   //open(accountLink.url);
   const accounts = await stripe.accounts.list({ limit: 10 });
-  console.log(accounts.data.map(account => [ account.id, account.first_name, account.last_name, account.email, account.country ]));
   runServer();
 };
 
