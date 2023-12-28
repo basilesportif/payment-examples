9e6a8c79b6bc01518855f9483c8868d35dcb7bde
timlucmiptev <info@timlucmiptev.space>
2023-12-28 14:22:51 +0100

    capture and refund flows work

--- fondy.js
+++ fondy.js
@@ -8,6 +8,7 @@ const TEST_MERCHANT_ID = '1396424';
 const TEST_PASSWORD = 'test';
 const API_ACCEPT_PAYMENT_FLOW_B = 'https://api.fondy.eu/api/checkout/url/';
 const API_CAPTURE_PAYMENT = 'https://api.fondy.eu/api/capture/order_id/';
+const API_REFUND_PAYMENT = 'https://api.fondy.eu/api/reverse/order_id/';
 
 // implements:  https://docs.fondy.eu/en/docs/page/3/#chapter-3-5
 // returns: original request with signature field added
@@ -42,17 +43,33 @@ const callFondyTest = async ({apiUrl, req}) => {
   return await res.json();
 };
 
-const capturePayment = async ({order_id, rectoken, amount, currency}) => {
+const capturePayment = async ({order_id, amount, currency}) => {
   const req = {
     order_id,
     merchant_id: TEST_MERCHANT_ID,
-    rectoken,
     amount,
     currency,
     version: "1.0",
   };
+  console.log(req);
   return await callFondyTest({
     apiUrl: API_CAPTURE_PAYMENT,
+    req
+  });
+};
+
+const refundPayment = async ({order_id, amount, currency}) => {
+  const req = {
+    order_id,
+    merchant_id: TEST_MERCHANT_ID,
+    amount,
+    currency,
+    version: "1.0",
+    comment: "stylist rejected booking",
+  };
+  console.log(req);
+  return await callFondyTest({
+    apiUrl: API_REFUND_PAYMENT,
     req
   });
 };
@@ -74,7 +91,6 @@ const withPreAuth = (fondyReq) => {
   return {
     ...fondyReq,
     preauth: 'Y',
-    required_rectoken: 'Y',
   };
 };
 /* **** end Fondy API Utils **** */
@@ -109,27 +125,61 @@ const runServer = () => {
     console.log(req.body);
     const strBody = JSON.stringify(req.body, undefined, 2);
     const order = getOrderDb(req.body.order_id);
-    updateOrderDb({...order, rectoken: req.body.rectoken});
+    updateOrderDb({ ...order, preauth: true, captured: false });
     res.status(200).send(
       `<html><body>
       <pre>${strBody}</pre>
       <a href="http://localhost:3000/accept_booking/${order.order_id}">
         <button>Accept Booking</button>
       </a>
+      <br/>
+      <a href="http://localhost:3000/reject_booking/${order.order_id}">
+        <button>Reject Booking</button>
+      </a>
       </body></html>`
     );
   });
-  app.get('/accept_booking/:order_id', (req, res) => {
+  app.get('/accept_booking/:order_id', async (req, res) => {
     console.log('accept_booking');
-    const order_id = req.params.order_id;
-    const strOrder = JSON.stringify(getOrderDb(order_id), undefined, 2);
+    const order = getOrderDb(req.params.order_id);
+    let strResult;
+    if (order.preauth) {
+      const result = await capturePayment(order);
+      strResult = JSON.stringify(result, undefined, 2);
+      updateOrderDb({ ...order, captured: true });
+    }
+    else {
+      strResult = "preauth not done; can't capture payment";
+    }
+    const strOrder = JSON.stringify(getOrderDb(req.params.order_id), undefined, 2);
     res.status(200).send(
       `<html><body>
       <pre>${strOrder}</pre>
+      <pre>${strResult}</pre>
       </body></html>`
     );
   });
-  
+  app.get('/reject_booking/:order_id', async (req, res) => {
+    console.log('reject_booking');
+    const order = getOrderDb(req.params.order_id);
+    let strResult;
+    if (order.preauth) {
+      const result = await refundPayment(order);
+      strResult = JSON.stringify(result, undefined, 2);
+      updateOrderDb({ ...order, captured: false, refunded: true });
+    }
+    else {
+      strResult = "preauth not done; can't refund payment";
+    }
+    const strOrder = JSON.stringify(getOrderDb(req.params.order_id), undefined, 2);
+    res.status(200).send(
+      `<html><body>
+      <pre>${strOrder}</pre>
+      <pre>${strResult}</pre>
+      </body></html>`
+    );
+  });
+
   app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
   });
